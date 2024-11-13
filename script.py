86cd4412c8a98cdecb5a21de90c1edb07296678c
DOMENICALI leonardo <leonardo.domenicali@etu.u-paris.fr>
2024-11-13 10:43:37 +0100

    added angle calulations

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def calibrate():
     image = cv2.imread("calibration.jpg")
@@ -90,30 +91,48 @@ def main():
     print("calibration is done, place pins\n")
     cv2.waitKey(0)
     #get_image("bowling_pins.jpg")
-    center_point=compute_center()
+    distance_factor = 24/5 # mesured on the board
+    bowling_pins_center=compute_center()
     calibration_distance = ((calibration_points[0][0] - calibration_points[1][0])**2 + (calibration_points[0][1] - calibration_points[1][1])**2)**0.5
-    center_line = calibration_distance*(24//5)
     image = cv2.imread("bowling_pins.jpg")
     
+    #calculate angle of the bottom line
     delta_x = calibration_points[1][0] - calibration_points[0][0]
     delta_y = calibration_points[1][1] - calibration_points[0][1]
-    angle = np.arctan2(delta_y, delta_x)
+    bottom_line_angle = np.arctan2(delta_y, delta_x)
 
-    # Calculate the middle point
-    middle_point = ((calibration_points[0][0] + calibration_points[1][0]) // 2, (calibration_points[0][1] + calibration_points[1][1]) // 2)
+    # Calculate the middle point of the bottom line
+    bottom_line_center_point = ((calibration_points[0][0] + calibration_points[1][0]) // 2, (calibration_points[0][1] + calibration_points[1][1]) // 2)
 
-    # Calculate the second point using the angle
-    length = calibration_distance*24/5  # Length of the line
-    ball_point = (int(middle_point[0] + length * np.sin(-angle)), int(middle_point[1] + length * np.cos(-angle)))
+    # Calculate the ball point using the angle knowing the distance from the center of the bottom line
+    length = calibration_distance*distance_factor  # Length of the line
+    ball_point = (int(bottom_line_center_point[0] + length * np.sin(-bottom_line_angle)), int(bottom_line_center_point[1] + length * np.cos(-bottom_line_angle)))
+    # Calculate the vector from calibration_point[0] to calibration_point[1]
+    line_vector = np.array([calibration_points[1][0] - calibration_points[0][0], calibration_points[1][1] - calibration_points[0][1]])
+    center_vector = np.array([bowling_pins_center[0] - calibration_points[0][0], bowling_pins_center[1] - calibration_points[0][1]])
+    # Project the center_vector onto the line_vector
+    line_length_squared = line_vector.dot(line_vector)
+    projection_length = center_vector.dot(line_vector) / line_length_squared
+    projection_vector = projection_length * line_vector
+    # Calculate the coordinates of the projected point
+    projected_point = (calibration_points[0][0] + projection_vector[0], calibration_points[0][1] + projection_vector[1])
+    projected_point = (int(projected_point[0]), int(projected_point[1]))
 
+    bottom_line_center_point=((calibration_points[0][0]+calibration_points[1][0])//2, (calibration_points[0][1]+calibration_points[1][1])//2)
 
-    middle_point=((calibration_points[0][0]+calibration_points[1][0])//2, (calibration_points[0][1]+calibration_points[1][1])//2)
-    cv2.circle(image, center_point, 5, (0, 255, 0), -1)  # Green dots on detected points
-    cv2.line(image, middle_point, ball_point, (0, 0, 255), 2)
-    cv2.line(image, calibration_points[0], calibration_points[1], (0, 0, 255), 2)
-    cv2.line(image,calibration_points[0],ball_point,(0,0,255),2)
-    cv2.line(image,calibration_points[1],ball_point,(0,0,255),2)
-    cv2.line(image,center_point,ball_point,(0,255,0),3)
+    cv2.circle(image, bowling_pins_center, 5, (0, 255, 0), -1)  # Green dots on center of the pins
+    cv2.line(image, bottom_line_center_point, ball_point, (0, 0, 255), 2) # Red line from the center of the bottom line to the ball
+    cv2.line(image, calibration_points[0], calibration_points[1], (0, 0, 255), 2) # Red line between the calibration points
+    cv2.line(image,calibration_points[0],ball_point,(0,0,255),2) # Red line from the first calibration point to the ball
+    cv2.line(image,calibration_points[1],ball_point,(0,0,255),2)# Red line from the second calibration point to the ball
+    cv2.line(image,bowling_pins_center,ball_point,(0,255,0),3)# Green line on the trajectory
+    cv2.circle(image, projected_point, 5, (255, 255, 0), -1)  # Red dot at the projected point
+    cv2.line(image, ball_point, projected_point, (255, 0, 0), 1)#blue line for the trajectory till the projected point
+
+    #calculate the angle of fireng 
+    fireng_angle =np.degrees(np.arctan(math.dist(projected_point,bottom_line_center_point)/math.dist(bottom_line_center_point,ball_point)))
+    cv2.putText(image, f"Fireng angle: {fireng_angle}", (7, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+
     cv2.imshow("Detected Points and Center", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
